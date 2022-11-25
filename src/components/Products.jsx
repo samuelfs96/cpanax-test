d96ad9784cc6fed7572add9ebe23b2933f520114
Samuel <samuelfs96@gmail.com>
2022-11-24 23:13:59 -0400

    responsive cards

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,7 @@ import { ListBulletIcon } from '@heroicons/react/24/solid'
 import { getProducts } from '../services/api'
 import { useState } from 'react'
 import SwitchMaxItems from './SwitchMaxItems'
+import Skeleton from './Skeleton'
 
 const Products = () => {
   const [maxItems, setMaxItems] = useState(4)
@@ -12,8 +13,6 @@ const Products = () => {
   const handleMaxItems = (item) => {
     setMaxItems(item)
   }
-
-  if (isLoading) return 'Loading...'
   if (error) return 'An error has occurred: ' + error.message
   return (
     <>
@@ -21,15 +20,23 @@ const Products = () => {
             <div className='flex justify-between items-center'>
                 <h1 className='text-md mt-10 mb-10 flex items-center gap-2 text-gray-600'><ListBulletIcon className='w-5'/> <span>Products</span></h1>
                 <SwitchMaxItems
-                    total={data.total}
+                    total={data?.total}
                     maxItems={maxItems}
                     handleMaxItems={handleMaxItems}
                 />
             </div>
             
-            <div className='grid grid-cols-4 gap-4'>
+            <div className='grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1'>
                 {
-                    data.products?.map(({images, title, brand}) => (
+                    !isLoading ?
+                    <>
+                        <Skeleton height='400px'/>
+                        <Skeleton height='400px'/>
+                        <Skeleton height='400px'/>
+                        <Skeleton height='400px'/>
+                    </>
+                    :
+                    data?.products.map(({images, title, brand}) => (
                         <Card key={title} className="hover:cursor-pointer">
                             <img src={images[0]} alt={title} className="w-40" />
                             <h2>{title}</h2>
