54c158d3dcfacdc6d6a4c3ebf3c7edd5058055b4
Samuel <samuelfs96@gmail.com>
2022-11-24 23:54:15 -0400

    finish card design

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -14,21 +14,26 @@ const Products = () => {
     setMaxItems(item)
   }
   if (error) return 'An error has occurred: ' + error.message
+
   return (
     <>
         <div className="container mt-10 mb-20">
             <div className='flex justify-between items-center'>
                 <h1 className='text-md mt-10 mb-10 flex items-center gap-2 text-gray-600'><ListBulletIcon className='w-5'/> <span>Products</span></h1>
-                <SwitchMaxItems
-                    total={data?.total}
-                    maxItems={maxItems}
-                    handleMaxItems={handleMaxItems}
-                />
-            </div>
-            
-            <div className='grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1'>
                 {
-                    !isLoading ?
+                    isLoading ?
+                        <Skeleton width='90px' height='40px'/>
+                    :
+                    <SwitchMaxItems
+                        total={data?.total}
+                        maxItems={maxItems}
+                        handleMaxItems={handleMaxItems}
+                    />
+                }
+            </div>
+            <div className='grid grid-cols-4 gap-6 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1'>
+                {
+                    isLoading ?
                     <>
                         <Skeleton height='400px'/>
                         <Skeleton height='400px'/>
@@ -37,10 +42,14 @@ const Products = () => {
                     </>
                     :
                     data?.products.map(({images, title, brand}) => (
-                        <Card key={title} className="hover:cursor-pointer">
-                            <img src={images[0]} alt={title} className="w-40" />
-                            <h2>{title}</h2>
-                            <h4>{brand}</h4>
+                        <Card key={title} className="custom-box hover:cursor-pointer h-[400px]">
+                            <div className='h-1/2 flex justify-center items-center'>
+                                <img src={images[0]} alt={title} className="h-1/2" />
+                            </div>
+                            <div className='gap-2 h-1/2 flex flex-col items-center text-center'>
+                                <span className='text-xl'>{title}</span>
+                                <span className='border border-gray-300 bg-slate-50 pt-1 pb-1 pr-2 pl-2 rounded-md text-xs lowercase'>{brand}</span>
+                            </div>
                         </Card>
                     ))
                 }
