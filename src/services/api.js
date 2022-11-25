6678186541628a9bb1981be22bb4e9678c83f1d9
Samuel <samuelfs96@gmail.com>
2022-11-25 12:05:27 -0400

    skip param to getproducts

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api_url = 'https://dummyjson.com/'
 
-export const getProducts = async (maxItems) => {
-    const { data } = await axios.get(api_url + 'products?limit=' + maxItems)
+export const getProducts = async (maxItems, skip) => {
+    const { data } = await axios.get(api_url + 'products?limit=' + maxItems + '&skip=' + skip)
     return data
 }
