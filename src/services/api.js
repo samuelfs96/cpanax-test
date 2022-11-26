089682e9f45f636d962632726984ade792781fac
Samuel <samuelfs96@gmail.com>
2022-11-26 00:18:05 -0400

    add getUsers method from api

--- src/services/api.js
+++ src/services/api.js
@@ -6,3 +6,8 @@ export const getProducts = async (maxItems, skip) => {
     const { data } = await axios.get(api_url + 'products?limit=' + maxItems + '&skip=' + skip)
     return data
 }
+
+export const getUsers = async (maxItems, skip) => {
+    const { data } = await axios.get(api_url + 'users?limit=' + maxItems + '&skip=' + skip)
+    return data
+}
