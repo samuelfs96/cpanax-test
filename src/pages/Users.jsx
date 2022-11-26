91ac7396578e989dd3708de447877f1e0a8d063b
Samuel <samuelfs96@gmail.com>
2022-11-25 22:33:17 -0400

    react base table

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import BaseTable, { Column } from 'react-base-table'
+import 'react-base-table/styles.css'
 
 const Users = () => {
+    const data = [
+        {col0: 'adfadfad', col1: 'adfadfadf'},
+        {col0: 'adfadfad', col1: 'adfadfadf'},
+        {col0: 'adfadfad', col1: 'adfadfadf'},
+        {col0: 'adfadfad', col1: 'adfadfadf'},
+    ]
   return (
-    <div>Users</div>
+    <>
+    <BaseTable data={data} width={600} height={400}>
+        <Column key="col0" dataKey="col0" title='fsgsfg' width={100} />
+        <Column key="col1" dataKey="col1" title='fsgsfgf' width={100} />
+    </BaseTable>
+    </>
   )
 }
 
